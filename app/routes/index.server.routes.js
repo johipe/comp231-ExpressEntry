590c494181e540046c5f876600c4db71d6f62999
Chemcee Cherian <chemceem@gmail.com>
2016-03-29 13:54:21 -0400

    Added files via upload

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,7 +1,9 @@
 module.exports = function(app) {
     var index = require ('../controllers/index.server.controller.js');
-    app.get('/',index.renderHomepage);
     
+    // general routes
+    app.get('/',index.renderHomepage);
+
     app.route('/homepage').get(index.renderHomepage);
     
     app.route('/about').get(index.renderAbout);
@@ -11,10 +13,20 @@ module.exports = function(app) {
     app.route('/services').get(index.renderServices);
     
     app.route('/contact').get(index.renderContact);
+
+    //routing for services list
+     app.route('/immigrate').get(index.renderImmigrate);
      
+     app.route('/work').get(index.renderWork);
+     
+     app.route('/study').get(index.renderStudy);
+     
+    // routes for client 
     app.route('/cli_homepage').get(index.renderClihomepage);
-     
-    app.route('/law_homepage').get(index.renderLawhomepage);
-    
-    app.route('/createApplicant').get(index.renderLawCreateApplicant);
+
+    // routes for lawayer
+    app.route('/law_homepage').get(index.renderLawhomepage);    
+    app.route('/edit/:id').get(index.renderLawEditApplicant);
+    app.route('/create/:id').get(index.renderCreateAppPage);
+        
 };
